582dde21e44e6ef81dbb6b834f04f19bbc2de928
j1ter <daniyal.samat@narxoz.kz>
2023-05-26 19:06:13 +0600

    value

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than or equal to 50, add the scroll-header class to the header tag
+  if (window.scrollY >= 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/*=============== VALUE ACCORDION ===============*/
+const accorddionItems = document.querySelectorAll(".value__accordion-item");
+
+accorddionItems.forEach((item) => {
+  const accorddionHeader = item.querySelector(".value__accordion-header");
+
+  accorddionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accorddionContent = item.querySelector(".value__accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accorddionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accorddionContent.style.height = accorddionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
